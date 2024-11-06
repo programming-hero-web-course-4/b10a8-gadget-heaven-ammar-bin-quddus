4985500947aeb40f72321671f62ee6f606a611f3
ammar-bin-quddus <ammarcghs372@gmail.com>
2024-11-06 23:50:14 +0600

    changes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -9,21 +9,20 @@ const Footer = () => {
       <footer className="footer bg-base-200 text-base-content justify-between p-10">
         <nav>
           <h6 className="footer-title">Services</h6>
-          <a className="link link-hover">Branding</a>
-          <a className="link link-hover">Design</a>
-          <a className="link link-hover">Marketing</a>
-          <a className="link link-hover">Advertisement</a>
+          <a className="link link-hover">Product Support</a>
+          <a className="link link-hover">Order Tracking</a>
+          <a className="link link-hover">Shipping and Delivery</a>
+          <a className="link link-hover">Returns</a>
         </nav>
         <nav>
           <h6 className="footer-title">Company</h6>
           <a className="link link-hover">About us</a>
           <a className="link link-hover">Contact</a>
-          <a className="link link-hover">Jobs</a>
-          <a className="link link-hover">Press kit</a>
+          <a className="link link-hover">Career</a>
         </nav>
         <nav>
           <h6 className="footer-title">Legal</h6>
-          <a className="link link-hover">Terms of use</a>
+          <a className="link link-hover">Terms of Service</a>
           <a className="link link-hover">Privacy policy</a>
           <a className="link link-hover">Cookie policy</a>
         </nav>
